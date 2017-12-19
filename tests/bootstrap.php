acb70dbc3c06feb330b6cba283eaed032966fa0c
Kieran Brahney <kieran@supportpal.com>
2017-12-19 12:03:14 +0000

    PHP 7.2 support

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -58,3 +58,34 @@ if (!function_exists('getParamsFromUrl')) {
         return $parsedQuery;
     }
 }
+
+/*
+|--------------------------------------------------------------------------
+| PHPUnit 4/5/6 Support
+|--------------------------------------------------------------------------
+|
+| PHPUnit 6 introduced a breaking change that removed 
+| PHPUnit_Framework_TestCase as a base class, and replaced it with
+| \PHPUnit\Framework\TestCase 
+|
+*/
+
+if (! class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
+
+if (! class_exists('\PHPUnit_Framework_Assert') && class_exists('\PHPUnit\Framework\Assert')) {
+    class_alias('\PHPUnit\Framework\Assert', 'PHPUnit_Framework_Assert');
+}
+
+if (! class_exists('\PHPUnit_Framework_ExpectationFailedException') && class_exists('\PHPUnit\Framework\ExpectationFailedException')) {
+    class_alias('\PHPUnit\Framework\ExpectationFailedException', 'PHPUnit_Framework_ExpectationFailedException');
+}
+
+if (! class_exists('\PHPUnit_Framework_Constraint_Not') && class_exists('\PHPUnit\Framework\Constraint\LogicalNot')) {
+    class_alias('\PHPUnit\Framework\Constraint\LogicalNot', 'PHPUnit_Framework_Constraint_Not');
+}
+
+if (! class_exists('\PHPUnit_Framework_Constraint') && class_exists('\PHPUnit\Framework\Constraint\Constraint')) {
+    class_alias('\PHPUnit\Framework\Constraint\Constraint', 'PHPUnit_Framework_Constraint');
+}
